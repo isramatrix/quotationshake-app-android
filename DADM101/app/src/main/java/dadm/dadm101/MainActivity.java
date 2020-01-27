89afc64ef66c616dea470584244fa3fab7e43d12
Zaka <zabou@inf.upv.es>
2020-01-27 17:16:23 +0100

    Cambio de texto

--- DADM101/app/src/main/java/dadm/dadm101/MainActivity.java
+++ DADM101/app/src/main/java/dadm/dadm101/MainActivity.java
@@ -3,6 +3,7 @@ package dadm.dadm101;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvWelcome = findViewById(R.id.tvWelcome);
+        tvWelcome.setText("IT'S A ME MAAARIOOO!!");
     }
 }
