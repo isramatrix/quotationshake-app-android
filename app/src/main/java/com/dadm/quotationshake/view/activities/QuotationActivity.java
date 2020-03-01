8d06be7cc46a55bcb23247698c22b281f43d1afa
Kairobe <56040619+Kairobe@users.noreply.github.com>
2020-03-01 18:09:53 +0100

    Acabada práctica 2B

--- app/src/main/java/com/dadm/quotationshake/view/activities/QuotationActivity.java
+++ app/src/main/java/com/dadm/quotationshake/view/activities/QuotationActivity.java
@@ -1,11 +1,21 @@
 package com.dadm.quotationshake.view.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.dadm.quotationshake.R;
+import com.dadm.quotationshake.model.ModelViewAdapter;
+import com.dadm.quotationshake.model.Quotation;
+
+import java.util.ArrayList;
 
 public class QuotationActivity extends AppCompatActivity {
 
@@ -13,5 +23,21 @@ public class QuotationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotation);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.favoriteMenuItem: break;
+            case R.id.refreshMenuItem: break;
+            default: return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.quotation_activity_menu,menu);
+        return true;
     }
 }
